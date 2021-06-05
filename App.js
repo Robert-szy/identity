e81eb71962ccc591259f994359ef8c983a81f0b4
Robert Szyszka <robert.szyszka@outlook.com>
2021-06-05 23:17:08 +0200

    Add 'for the ambitious' code

--- App.js
+++ App.js
@@ -1,43 +1,78 @@
-//const os = require('os');
 const fs = require('fs');
 
 const genders = ['M', 'F'];
 const maleNames = ['Robert', 'Marek', 'Jurek', 'Paweł', 'Marcin']; 
 const femaleNames = ['Joanna', 'Katarzyna', 'Małgorzata', 'Zofia', 'Marlena'];
 const lastNames = ['Kowalski', 'Nowak', 'Doe', 'Fritz', 'Divine'];
+const emails = ['@gmail.com', '@live.com', '@hotmail.com', '@outlook.com', '@op.pl', '@interia.pl']
 const people = [];
 
 const randChoice = (arr) => {
     const min = 0;
     const max = arr.length - 1;
     const choice = arr[Math.floor(Math.random() * (max - min + 1)) + min];
-    console.log('choice: ', choice);
     return choice;
 };
 
-const drawPeople = function () {
-for (let i=1; i<=20; i++) {
-    const draw = [];
+const createEmail = (first, last) => {
+    const email = (first + '.' + last + randChoice(emails)).toLowerCase();
+    return email;
+}
+
+const createPhone = () => {
+    const phone = [];
     
-    const gender = (randChoice (genders));
-    draw.push(gender);
-
-    if (gender == 'M'){
-    draw.push(randChoice (maleNames));
-    } else {
-    draw.push(randChoice (femaleNames));
+    for (let i=1; i<=9; i++){
+        let number = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+        phone.push(number);
     }
-    draw.push(randChoice (lastNames));
 
-    const age = Math.floor(Math.random() * (100 + 1));
-    draw.push(age);
+    return phone.join('');
+}
 
-    people.push(draw);
-    console.log('people(i):', i, people);
-};
+const drawPeople = function () {
+    for (let i=1; i<=20; i++) {
+        const draw = [];
+        let first = '';
+        let last = '';
+        let phone = '';
+        
+        const gender = (randChoice (genders));
+        draw.push(gender);
 
-console.log('people', people);
-return;
+        if (gender == 'M'){
+            first = randChoice (maleNames);
+            draw.push(first);
+        } else {
+            first = randChoice (femaleNames);
+            draw.push(first);
+        }
+
+        last = randChoice (lastNames);
+        draw.push(last);
+
+        const email = createEmail(first, last);
+        draw.push(email);
+
+        const age = Math.floor(Math.random() * (100 - 18 + 1)) + 18;
+        draw.push(age);
+
+        phone = createPhone();
+        draw.push(phone);
+
+        people.push(draw);
+    };
+
+    console.log('people', people);
+
+    const jsonstring = JSON.stringify(people);
+
+    fs.writeFile('people.json', jsonstring, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+
+    return;
 }
 
 drawPeople ();
